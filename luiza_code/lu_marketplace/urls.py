5d78c9de74acf580488ebe454f2faf47cdf1912b
suele <suelen.ads.guimaraes@gmail.com>
2020-11-20 21:31:07 -0300

    Altera o nome da url para detalhar.

--- luiza_code/lu_marketplace/urls.py
+++ luiza_code/lu_marketplace/urls.py
@@ -7,15 +7,20 @@ urlpatterns = [
 
     path('', views.index, name='luMarketplace-index'),
 
-    path('cadastrar-prod/', CadastrarProduto.as_view(),name='luMarketplace-cadastro'),
+    path('cadastrar-prod/', CadastrarProduto.as_view(),
+         name='luMarketplace-cadastro'),
 
-    path('atualizar-prod/<int:id>', views.luMarketplace_atualizar,name='luMarketplace-atualizar'),
+    path('atualizar-prod/<int:id>', views.luMarketplace_atualizar,
+         name='luMarketplace-atualizar'),
 
-    path('luMarketplace_deletar/<id_prod>', views.luMarketplace_deletar, name='luMarketplace-deletar'),
-    
-    path('luMarketplace_inativar/<id_prod>', views.luMarketplace_inativar, name='luMarketplace-inativar'),
-    
-    path('luMarketplace_ativar/<id_prod>', views.luMarketplace_ativar, name='luMarketplace-ativar'),
+    path('luMarketplace_detalhar/<id_prod>',
+         views.luMarketplace_detalhar, name='luMarketplace-deletar'),
+
+    path('luMarketplace_inativar/<id_prod>',
+         views.luMarketplace_inativar, name='luMarketplace-inativar'),
+
+    path('luMarketplace_ativar/<id_prod>',
+         views.luMarketplace_ativar, name='luMarketplace-ativar'),
 
     path('login/', views.login_user),
 
