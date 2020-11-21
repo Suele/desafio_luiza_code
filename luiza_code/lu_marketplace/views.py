4dc30216c55d410b067b5c10b09c0fd84b8d7c3a
suele <suelen.ads.guimaraes@gmail.com>
2020-11-20 21:29:00 -0300

    Remove a funçao de deletar.

--- luiza_code/lu_marketplace/views.py
+++ luiza_code/lu_marketplace/views.py
@@ -7,8 +7,9 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from . forms import CadastraProduto
-from django.shortcuts import get_object_or_404,redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+
 
 @login_required(login_url='/login/')
 def index(request):
@@ -23,22 +24,23 @@ def index(request):
     return render(request, 'lu_marketplace/index.html', context)
 
 
-
 class CadastrarProduto(SuccessMessageMixin, CreateView):
     model = Produto
     template_name = 'cadastrar_prod.html'
     fields = '__all__'
     success_message = 'Produto cadastrado com sucesso!'
-    def get_success_url(self):  
+
+    def get_success_url(self):
         return '/'
+
 
 def luMarketplace_atualizar(request, id):
     p = get_object_or_404(Produto, pk=id)
     form = CadastraProduto(instance=p)
     if(request.method == 'POST'):
         form = CadastraProduto(request.POST, instance=p)
-        
-        if(form.is_valid()): #valida o formulário
+
+        if(form.is_valid()):  # valida o formulário
             p = form.save(commit=False)
             p.prod_nome = form.cleaned_data['prod_nome']
             p.prod_descricao = form.cleaned_data['prod_descricao']
@@ -49,16 +51,18 @@ def luMarketplace_atualizar(request, id):
             p.save()
             return redirect('/')
         else:
-            return render(request, 'atualizar_prod.html', {'form': form, 'p' : p})
+            return render(request, 'atualizar_prod.html', {'form': form, 'p': p})
     elif(request.method == 'GET'):
-        return render(request, 'atualizar_prod.html', {'form': form, 'p' : p})
-  
-def luMarketplace_deletar(request, id_prod):
-    Produto.objects.filter(prod_codigo=id_prod).delete()
+        return render(request, 'atualizar_prod.html', {'form': form, 'p': p})
+
+
+def luMarketplace_detalhar(request, id_prod):
+    Produto.objects.filter(prod_codigo=id_prod)
     produtos = Produto.objects.all()
     listaProdutos = Produto.objects.all()
     context = {'produtos': produtos, 'listaProdutos': listaProdutos}
-    return render(request, 'lu_marketplace/index.html', context)
+    return render(request, 'lu_marketplace/detalhes.html', context)
+
 
 def luMarketplace_inativar(request, id_prod):
     Produto.objects.filter(prod_codigo=id_prod).update(prod_inativo=True)
@@ -67,12 +71,14 @@ def luMarketplace_inativar(request, id_prod):
     context = {'produtos': produtos, 'listaProdutos': listaProdutos}
     return render(request, 'lu_marketplace/index.html', context)
 
+
 def luMarketplace_ativar(request, id_prod):
     Produto.objects.filter(prod_codigo=id_prod).update(prod_inativo=False)
     produtos = Produto.objects.all()
     listaProdutos = Produto.objects.all()
     context = {'produtos': produtos, 'listaProdutos': listaProdutos}
     return render(request, 'lu_marketplace/index.html', context)
+
 
 def login_user(request):
     return render(request, 'login.html')
@@ -81,14 +87,15 @@ def login_user(request):
 @csrf_protect
 def submit_login(request):
     if request.POST:
-        user=request.POST.get('user')
-        senha=request.POST.get('senha')
-        user = authenticate(username=user, password = senha)
+        user = request.POST.get('user')
+        senha = request.POST.get('senha')
+        user = authenticate(username=user, password=senha)
         if user is not None:
             login(request, user)
             return redirect('/')
         else:
-            messages.error(request, "Usuário/Senha incorretas, favor tentar novamente")
+            messages.error(
+                request, "Usuário/Senha incorretas, favor tentar novamente")
     return redirect('/login/')
 
 
